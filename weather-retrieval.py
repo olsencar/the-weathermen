f8f9a1bc6659f79f85030b05737fd3ff2275f89b
Carter <olsencar@oregonstate.edu>
2018-07-31 23:57:59 -0700

    Added more data to scrape and some fixes to the code.

--- weather-retrieval.py
+++ weather-retrieval.py
@@ -9,7 +9,7 @@ API_ENDPOINT = "http://api.worldweatheronline.com/premium/v1/past-weather.ashx"
 DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 class Weather:
-    def __init__(self, date, city, state, avgHigh, avgLow, uvIndex, totalSunHours, avgSunHours, totalSnow, avgSnow):
+    def __init__(self, date, city, state, avgHigh, avgLow, uvIndex, totalSunHours, avgSunHours, totalSnow, avgSnow, totalRainfall, avgRainfall, avgHumidity, avgPressure, avgWindSpeed):
         self.date = date
         self.city = city
         self.state = state
@@ -20,6 +20,12 @@ class Weather:
         self.avgSunHours = avgSunHours
         self.totalSnow = totalSnow
         self.avgSnow = avgSnow
+        self.totalRainfall = totalRainfall
+        self.avgRainfall = avgRainfall
+        self.avgHumidity = avgHumidity
+        self.avgPressure = avgPressure
+        self.avgWindSpeed = avgWindSpeed
+
 
 def get_api_count():
     file = open("api_count.txt", "r")
@@ -48,12 +54,20 @@ def createNewWeather(response, month, year, city, state):
     avgSun = 0
     avgUV = 0
     avgSnow = 0
-    
+    avgRainfall = 0
+    avgHumidity = 0
+    avgPressure = 0
+    avgWindSpeed = 0
+
     totalHigh = 0
     totalLow = 0
     totalUV = 0
     totalSun = 0
     totalSnow = 0
+    totalRainfall = 0
+    totalHumidity = 0
+    totalPressure = 0
+    totalWindSpeed = 0
     numOfDays = 0
 
     for i in response['data']['weather']:
@@ -62,19 +76,28 @@ def createNewWeather(response, month, year, city, state):
         totalUV += int(i['uvIndex'])
         totalSun += float(i['sunHour'])
         totalSnow += float(i['totalSnow_cm'])
+        totalRainfall += float(i['hourly'][0]['precipMM'])
+        totalHumidity += int(i['hourly'][0]['humidity'])
+        totalPressure += int(i['hourly'][0]['pressure'])
+        totalWindSpeed += int(i['hourly'][0]['WindGustMiles'])
         numOfDays += 1
-
+        
+    totalRainfall = round(totalRainfall * 0.0393701, 2)
     avgHigh = round(totalHigh / numOfDays, 2)
     avgLow = round(totalLow / numOfDays, 2)
     avgSnow = round(totalSnow / numOfDays, 2)
     avgSun = round(totalSun / numOfDays, 2)
     avgUV = round(totalUV / numOfDays, 2)
+    avgRainfall = round(totalRainfall / numOfDays, 2)
+    avgHumidity = round(totalHumidity / numOfDays, 2)
+    avgPressure = round(totalPressure / numOfDays, 2)
+    avgWindSpeed = round(totalWindSpeed / numOfDays, 2)
     totalSun = round(totalSun, 2)
     totalSnow = round(totalSnow, 2)
 
     date = "{}/{}".format(year, month)
 
-    return Weather(date, city, state, avgHigh, avgLow, avgUV, totalSun, avgSun, totalSnow, avgSnow)
+    return Weather(date, city, state, avgHigh, avgLow, avgUV, totalSun, avgSun, totalSnow, avgSnow, totalRainfall, avgRainfall, avgHumidity, avgPressure, avgWindSpeed)
 
 def getCitiesAndStates(CITIES, STATES, indexStart, indexEnd):
     if (indexStart == 0):
@@ -95,9 +118,9 @@ def main():
 
     print("\nMake sure to enter index as if you were accessing data in an array by it's index. arr[0] is 1st element.")
     indexStart = int(input("Starting city index (0, 100, 200): "))
-
-    print("\nWe are limited to retrieving a years worth of data for 39 cities every day.")
-    print("If your start index is 0, make your end index 39")
+    currentIndex = indexStart
+    print("\nWe are limited to retrieving a years worth of data for 38 cities every day.")
+    print("If your start index is 0, make your end index 38")
     indexEnd = int(input("Ending city index: "))
 
     getCitiesAndStates(CITIES, STATES, indexStart, indexEnd)
@@ -118,6 +141,7 @@ def main():
                 "key": API_KEY,
                 "date": "{}/{}/01".format(year, month),
                 "enddate": getEndDate(month, year),
+                "tp": "24",
                 "format": "json"
             }
             response = requests.get(API_ENDPOINT, params=parameters)
@@ -126,18 +150,18 @@ def main():
                 print("Bad response from API. You have most likely reached your limit for the day.")
                 return
             else:
-                print("Successful response from API on date: {}/{} | index: {} | {}, {}".format(year, month, indexStart, city, state), flush=True)
+                print("Successful response from API on date: {}/{} | index: {} | {}, {}".format(year, month, currentIndex, city, state), flush=True)
 
             data = json.loads(response.content)
             w = createNewWeather(data, month, year, city, state)
 
-            file = open("weather-data.csv", "a")
-            file.write("{},{},{},{},{},{},{},{},{},{},{}\n".format(indexStart, w.city, w.state, w.date, w.avgHigh, w.avgLow, w.uvIndex, w.totalSunHours, w.avgSunHours, w.totalSnow, w.avgSnow))
+            file = open("weather-data-new.csv", "a")
+            file.write("{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n".format(currentIndex, w.city, w.state, w.date, w.avgHigh, w.avgLow, w.uvIndex, w.totalSunHours, w.avgSunHours, w.totalSnow, w.avgSnow, w.totalRainfall, w.avgRainfall, w.avgHumidity, w.avgPressure, w.avgWindSpeed))
             file.close()
         
             month += 1
             time.sleep(2)
-        indexStart += 1
+        currentIndex += 1
         cityCount += 1
     
 if __name__ == '__main__':
