92c5b7d65f87049f66d4ce9ebf540413e5e88d05
Carter Olsen <olsencar@oregonstate.edu>
2018-08-14 13:14:36 -0700

    Got results and city communicating

--- web/server.py
+++ web/server.py
@@ -12,7 +12,7 @@ from whoosh.searching import *
 app = Flask(__name__)
 
 class City:
-    def __init__(self, city, state, date, avgHigh, avgLow, uvIndex, totalSunHours, avgSunHours, totalSnow, avgSnow, totalRainfall, avgRainfall, avgHumidity, avgPressure, avgWindSpeed):
+    def __init__(self, city, state, date, avgHigh, avgLow, uvIndex, totalSunHours, avgSunHours, totalSnow, avgSnow, totalRainfall, avgRainfall, avgHumidity, avgPressure, avgWindSpeed, avgTemp):
         self.date = date
         self.city = city
         self.state = state
@@ -28,6 +28,7 @@ class City:
         self.avgHumidity = avgHumidity
         self.avgPressure = avgPressure
         self.avgWindSpeed = avgWindSpeed
+        self.avgTemp = avgTemp
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -85,37 +86,41 @@ def results():
         nr = NumericRange("avgTemp", int(minTemp), int(maxTemp));
         # np = query.Term("Index", 151);
         
-        results = searcher.search(queryTest, filter=nr, limit=None)
-        print("Length of results: " + str(len(results)))
+        r = searcher.search(queryTest, filter=nr, limit=None)
+        print("Length of results: " + str(len(r)))
+        arr = []
         Cities = {}
-        for line in results:
+        for line in r:
             if line['City'] in Cities:
                 pass;
             else:
                 Cities[line['City']] = line['State'];
-        
+            arr.append(City(line['City'], line['State'], datetime.strftime(line['Date'], "%Y-%m"), line['avgHigh'], line['avgLow'],line['avgUV'], line['totalSun'], line['avgSun'],line['totalSnow'], line['avgSnow'], line['totalRainfall'], line['avgRainfall'], line['avgHumidity'], line['pressure'], line['windSpeed'], line['avgTemp']))
         for key in Cities:
             print("City: " + key + " State: " + Cities[key]);
 
     print("You searched for: " + query)
+    if (len(Cities) > 1):
+        return render_template('results.html', query=query, results=Cities, searchterm=searchTerm)    
     
-    return render_template('results.html', query=query)
+    return render_template('city.html', query=query, results=arr, searchterm=searchTerm)
 
 
-@app.route('/city/', methods=['GET', 'POST'])
+@app.route('/city', methods=['GET', 'POST'])
 def city():
     data = request.args
 
-    cityName = data.get('city')
-    stateName = data.get('state')
-
+    # cityName = data.get('city')
+    # stateName = data.get('state')
+    searchterm = data.get('searchterm')
+    # print(searchterm)
     # Get data about city from query
     city = []
-    city.append(City("New York", "New York", "2017/7", 88.48, 77.0,
-                     0.0, 0.0, 0.0, 0.0, 0.0, 3.94, 0.13, 64.42, 1014.68, 10.26))
-    city.append(City("New York", "New York", "2017/8", 88.48, 77.0,
-                     0.0, 0.0, 0.0, 0.0, 0.0, 3.94, 0.13, 64.42, 1014.68, 10.26))
-    return render_template('city.html', city=city)
+    # city.append(City("New York", "New York", "2017/7", 88.48, 77.0,
+    #                  0.0, 0.0, 0.0, 0.0, 0.0, 3.94, 0.13, 64.42, 1014.68, 10.26))
+    # city.append(City("New York", "New York", "2017/8", 88.48, 77.0,
+    #                  0.0, 0.0, 0.0, 0.0, 0.0, 3.94, 0.13, 64.42, 1014.68, 10.26))
+    return render_template('city.html', city=searchterm)
 
 
 if (__name__ == '__main__'):
